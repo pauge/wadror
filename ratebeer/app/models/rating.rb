236bd3b8a0cef0ddd91e258bfd968a4afc30f796
Arto Kiviluoto <arto.kiviluoto@gmail.com>
2013-12-28 22:22:04 +0200

    W3 task 3 done

--- ratebeer/app/models/rating.rb
+++ ratebeer/app/models/rating.rb
@@ -1,6 +1,7 @@
 class Rating < ActiveRecord::Base
   attr_accessible :beer_id, :score
   belongs_to :beer
+	belongs_to :user
 
   def to_s
 	b = Beer.find_by_id "#{beer_id}"
--- ratebeer/db/schema.rb
+++ ratebeer/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131228165424) do
+ActiveRecord::Schema.define(:version => 20131228201653) do
 
   create_table "beers", :force => true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20131228165424) do
     t.integer  "beer_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
