6447387e2397f8acef28e0d71f415205b6b162e9
Arto Kiviluoto <arto.kiviluoto@gmail.com>
2013-12-30 21:51:39 +0200

    Week3 done

--- ratebeer/app/models/brewery.rb
+++ ratebeer/app/models/brewery.rb
@@ -3,12 +3,18 @@ class Brewery < ActiveRecord::Base
 
   attr_accessible :name, :year
 
-	validates_presence_of :name
+	validates_presence_of :name, :year
 	validates_numericality_of :year, { :greater_than_or_equal_to => 1042,
-																		:less_than_or_equal_to => 2013,
-																		:only_integer => true }
+																		  :only_integer => true }
+	validate :year_not_in_future
 
   has_many :beers
   has_many :ratings, :through => :beers
 
+
+	def year_not_in_future
+		if self.year > Integer(Time.now.year)
+			errors.add(:year, "can't be more than '#{Time.now.year}'")
+		end
+	end
 end
